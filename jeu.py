e93296626767d9c9f3737c6a1510d5c2f65e0afb
Axel Flahaut <axel.flahaut@ecoles-epsi.net>
2024-02-08 16:16:24 +0100

    juste Prix V3

--- jeu.py
+++ jeu.py
@@ -1,38 +1,51 @@
 import random
+tentatives=0
 
 def level1():
     r=random.randint(0,100)
     nombre=int(input("Entrez un nombre entre 0 et 100: "))
+    global tentatives
+    tentatives+=1
     while nombre!=r: 
         if nombre<r:
             print("C'est plus")
         else:
             print("C'est moins")
         nombre=int(input("Entrez un nombre entre 0 et 100: "))
-
-    print("Bravo, vous avez trouvé le nombre mystère !")
+        tentatives+=1 
+        
+    print("Bravo, vous avez trouvé le nombre mystère en", tentatives, "tentatives !")
 
 def level2():
     r=random.randint(0,1000)
     nombre=int(input("Entrez un nombre entre 0 et 1000: "))
+    global tentatives
+    tentatives+=1
     while nombre!=r:
         if nombre<r:
             print("C'est plus")
         else:
             print("c'est moins")
         nombre=int(input("Entrez un nombre entre 0 et 1000: "))
-    print("Bravo, vous avez trouvé le nombre mystère !")
+        tentatives+=1
+    print("Bravo, vous avez trouvé le nombre mystère en", tentatives, "tentatives !")
+    
+
 
 def level3():
     r=random.randint(0,100000)
     nombre=int(input("Entrez un nombre entre 0 et 100000: "))
+    global tentatives
+    tentatives+=1
     while nombre!=r:
         if nombre<r:
             print("C'est plus")
-        else: 
+        else:
             print("C'est moins")
-        nombre:int(input("Entrez un nombre entre 0 et 100000: "))
-    print("Bravo, vous avez trouvé le nombre mystère !")
+        nombre=int(input("Entrez un nombre entre 0 et 100000: "))
+        tentatives+=1
+    print("Bravo, vous avez trouvé le nombre mystère en", tentatives, "tentatives !")
+
 
 
 level=int(input("Choisissez un niveau de difficulté (1, 2 ou 3): "))
